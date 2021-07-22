3aff09f14a42db61e308b036c1682a3965210d3f
Angel Angelov <flushbg@gmail.com>
2021-07-22 16:01:56 +0300

    Establish connection to Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      // TODO: Move to proper configuration management
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      // Bad for production, better use migrations
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
